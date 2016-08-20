0f9fc3405cfb8e6aa95e694cfecebe7630e986bb
Josh Cheek <josh.cheek@gmail.com>
2016-08-20 03:45:22 -0500

    Well, that was way the fuck easier than the objecty thing I was doing before

--- promises.js
+++ promises.js
@@ -1,67 +1,62 @@
 module.exports = MyPromise
 
 function MyPromise(fn) {
-  this._thenClauses = []
-  this._catchBlocks = []
+  var state     = 'pending'
+  var result    = undefined
+  var callbacks = []
+
+  this.then = function(fn) {
+    return new Promise((resolve, reject) => {
+      var invoke = () => {
+        if (state === 'settled') resolve(fn(result))
+        else if (state === 'error') reject(result)
+      }
+      if(state !== 'pending') invoke()
+      else callbacks.push(invoke)
+    })
+  }
+
+  this.catch = function(fn) {
+    return new Promise((resolve, reject) => {
+      var invoke = () => {
+        if (state === 'error') resolve(fn(result))
+        else if (state === 'settled') resolve(result)
+      }
+      if(state !== 'pending') invoke()
+      else callbacks.push(invoke)
+    })
+  }
+
+  function invokeCallbacks() {
+    callbacks.forEach(cb => cb())
+    callbacks = "You should not have gotten this far"
+  }
+
   const resolve = invokeOnlyOnce(val => {
-    this._resolvedTo = val
-    this._thenClauses.forEach(fn => fn(val))
+    executeLater(() => {
+      state  = 'settled'
+      result = val
+      invokeCallbacks()
+    })
   })
-  const reject  = (idk) => console.log("Uhhhh, this is a mistake >.<")
+
+  const reject = (val) => { // invoke only once?
+    // execute later?
+    state  = 'error'
+    result = val
+    invokeCallbacks()
+  }
+
   try {
     fn(resolve, reject)
   } catch(err) {
-    this._errorTo = err;
-    this._catchBlocks.forEach(fn => fn(err))
+    reject(err)
   } finally {
     // something probably goes here .done, maybe?
   }
 }
 
-MyPromise.prototype.then = function(fn) {
-  var caught = null
-  this.catch(err => this.caught = null)
-  return new MyPromise((resolve, reject) => {
-    var invoke = buildThenInvocation(fn, resolve, reject)
-    if(caught)
-      raise(caught)
-    else if(this._resolvedTo !== undefined) // TODO: should we be able to resolve to undefined?
-      invoke(this._resolvedTo)
-    else
-      this._thenClauses.push(invoke)
-  })
-}
-
-MyPromise.prototype.catch = function(fn) {
-  return new MyPromise((resolve, reject) => {
-    var invoke = buildThenInvocation(fn, resolve, reject)
-    if(this._errorTo !== undefined) // TODO: should we be able to have an error of undefined?
-      invoke(this._errorTo)
-    else if(this._resolvedTo !== undefined) // TODO: should we be able to resolve to undefined?
-      invoke(this._resolvedTo)
-    else
-      this._catchBlocks.push(invoke)
-  })
-}
-
 const executeLater = function(fn) { setTimeout(fn, 0) }
-
-function buildThenInvocation(thenClause, resolve, reject) {
-  return (val, err) => {
-    executeLater(() => {
-      if(err) {
-        promise._catchBlocks.forEach(fn => fn(val))
-        reject(err)
-      } else {
-        var result = thenClause(val)
-        if(result && result.constructor === MyPromise)
-          result.then(val => resolve(val))
-        else
-          resolve(result)
-      }
-    })
-  }
-}
 
 function invokeOnlyOnce(fn) {
   var invoked = false;
