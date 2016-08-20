f51261b16ab725c38bdbc29ea824abb8b127abb5
Josh Cheek <josh.cheek@gmail.com>
2016-08-19 21:08:44 -0500

    It invokes the constructor function with callbacks for resolution and rejection

--- test/promises_test.js
+++ test/promises_test.js
@@ -5,19 +5,32 @@ const MyPromise = require('../promises.js')
 console.log(MyPromise)
 
 describe('MyPromise', function() {
-  it('is constructed with a function that it invokes immediately', function() {
-    var value = "unchanged"
-    new MyPromise((resolve, reject) => value = "changed")
-    assert.equal("changed", value)
+  it('is constructed with a function, which it immediately invokes with resolution and rejection callbacks', function() {
+    var resolve_cb = "not yet set";
+    var reject_cb  = "not yet set";
+    new MyPromise((resolve, reject) => {
+      resolve_cb = resolve
+      reject_cb  = reject
+    })
+    assert.equal(resolve_cb.constructor, Function)
+    assert.equal(reject_cb.constructor,  Function)
   })
 
-  it('gives the function a function for resolution and rejection')
-  it('invokes waiting functions when its resolution function has been called')
-  it('has a then method that will be used to add waiting functions')
-  it('passes the result of one waiting function as the value for the next waiting function')
-  it('invokes waiting functions in a breadth-first manner')
+  it.skip('invokes waiting functions when its resolution function has been called', function() {
+  })
+
+  it.skip('has a then method that will be used to add waiting functions', function() {
+  })
+
+  it.skip('passes the result of one waiting function as the value for the next waiting function', function() {
+  })
+
+  it.skip('invokes waiting functions in a breadth-first manner', function() {
+  })
+
   describe('when the result is a promise', function() {
-    it('passes the promise\'s result as the next value, not the promise itself')
+    it.skip('passes the promise\'s result as the next value, not the promise itself', function() {
+    })
   })
   // assert.deepEqual(expected, this.rejs.getTable('testOne'))
 })
